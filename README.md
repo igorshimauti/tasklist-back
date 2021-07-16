5476209dd71868e33df87f5fb5af99ad88836d17
igor.shimauti <igor.shimauti@linx.com.br>
2021-07-15 23:08:29 -0300

    Add swagger. Add Spring security.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tasklist-Supero
+
+Projeto Back-end, desenvolvido com Spring Boot, JPA, Spring Security, Fly Way, MySQL e documentação com Swagger.
--- src/main/java/br/com/tasklist/TasklistApplication.java
+++ src/main/java/br/com/tasklist/TasklistApplication.java
@@ -1,7 +1,13 @@
 package br.com.tasklist;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class TasklistApplication {
@@ -9,5 +15,13 @@ public class TasklistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasklistApplication.class, args);
 	}
-
+	
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description) {
+		return new OpenAPI().info(new Info()
+				.title(description)
+				.version("1.0")
+				.termsOfService("http://swagger.io.terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));			
+	}
 }
